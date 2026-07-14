9098571f63d7eacd7887446c105bbf15735a518e
Asif Ansari <ansariasif4911@gmail.com>
2026-07-14 23:58:49 +0530

    signup page setup

--- src/app/features/auth/signup/signup.ts
+++ src/app/features/auth/signup/signup.ts
@@ -1,9 +1,44 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
+function passwordsMatch(control: AbstractControl): ValidationErrors | null {
+  const password = control.get('password')?.value;
+  const confirmPassword = control.get('confirmPassword')?.value;
+  return password === confirmPassword ? null : { passwordMismatch: true };
+}
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './signup.html',
   styleUrl: './signup.scss',
+  standalone: true,
 })
-export class Signup {}
+export class Signup {
+  signupForm!: any;
+
+  constructor(private fb: FormBuilder) {}
+
+
+  ngOnInit() {
+    this.signupForm = this.fb.group({
+      fullName: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+    }, { validators: passwordsMatch });
+  }
+
+  get fullName() {return this.signupForm.get('fullName')}
+  get email() { return this.signupForm.get('email'); }
+  get password() { return this.signupForm.get('password'); }
+  get confirmPassword() { return this.signupForm.get('confirmPassword'); }
+
+  onSubmit() {
+    if (this.signupForm.invalid) {
+      this.signupForm.markAllAsTouched();
+      return;
+    }
+    console.log('Signup form value:', this.signupForm.value);
+  }
+}
